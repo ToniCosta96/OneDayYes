eb10affeaf72fdec94d9a8345bc2eb38e396cc59
ToniCosta <ToniCosta96@gmail.com>
2018-02-08 13:16:28 +0100

    control de posicion en el menú lateral

--- src/UserBundle/Controller/AdminController.php
+++ src/UserBundle/Controller/AdminController.php
@@ -12,7 +12,7 @@ class AdminController extends Controller
     /**
      * @Route("/admin/", name="admin")
      */
-    public function adminAction(Request $request)
+    public function adminAction()
     {
         $repository = $this->getDoctrine()->getRepository(User::class);
         $usuarios = $repository->findAll();
@@ -23,6 +23,46 @@ class AdminController extends Controller
      * @Route("/admin/usuarios", name="admin_usuarios")
      */
     public function adminUsuariosAction(Request $request)
+    {
+        $repository = $this->getDoctrine()->getRepository(User::class);
+        $usuarios = $repository->findAll();
+        return $this->render('@User/Admin/admin.html.twig', array('usuarios'=>$usuarios));
+    }
+
+    /**
+     * @Route("/admin/reservas", name="admin_reservas")
+     */
+    public function adminReservasAction(Request $request)
+    {
+        $repository = $this->getDoctrine()->getRepository(User::class);
+        $usuarios = $repository->findAll();
+        return $this->render('@User/Admin/admin.html.twig', array('usuarios'=>$usuarios));
+    }
+
+    /**
+     * @Route("/admin/actividades", name="admin_actividades")
+     */
+    public function adminActividadesAction(Request $request)
+    {
+        $repository = $this->getDoctrine()->getRepository(User::class);
+        $usuarios = $repository->findAll();
+        return $this->render('@User/Admin/admin.html.twig', array('usuarios'=>$usuarios));
+    }
+
+    /**
+     * @Route("/admin/descuentos", name="admin_descuentos")
+     */
+    public function adminDescuentosAction(Request $request)
+    {
+        $repository = $this->getDoctrine()->getRepository(User::class);
+        $usuarios = $repository->findAll();
+        return $this->render('@User/Admin/admin.html.twig', array('usuarios'=>$usuarios));
+    }
+
+    /**
+     * @Route("/admin/mensajes", name="admin_mensajes")
+     */
+    public function adminMensajesAction(Request $request)
     {
         $repository = $this->getDoctrine()->getRepository(User::class);
         $usuarios = $repository->findAll();
